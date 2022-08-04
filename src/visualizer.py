25a27f1b4c58bc2f4de70d67ec14a2b53ce0a140
Alexander van Schie (Student) <alexander.vanschie@stud.fhgr.ch>
2022-08-04 09:55:28 +0200

    removed unused packages

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,6 +1,3 @@
-import seaborn
-import matplotlib.pyplot as plt
-import pandas
 import json
 
 fns = ['Morning_run', 'Lunch_Run']
@@ -19,10 +16,3 @@ for fn in fns:
             print(i, segment)
     print(f'Average performance: {sum(performances) / len(performances)}')
     print(f'Average gap: {sum(gaps) / len(gaps)}')
-#segments = pandas.DataFrame.from_dict(data['segments'], orient='index')
-#segments['km'] = segments.index.astype('int') + 1
-#segments['min'] = segments.index.astype('int') + 1
-
-#seaborn.set_theme(style='darkgrid')
-#seaborn.lineplot(x='min', y='performance', data=segments)
-#plt.show()
